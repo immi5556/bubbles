75e0ddb04727c02e65ba5c0a9dfab9cb5a652f39
Immanuel Raj <kimr@solar.dk>
2025-03-22 21:27:25 +0530

    more changes.

--- web_rtc/signal_node/app.js
+++ web_rtc/signal_node/app.js
@@ -1,9 +1,5 @@
-
-//const express = require('express');
 import express from 'express'
-//const http = require('http');
 import * as http from 'http';
-//const WebSocket = require('ws');
 import { WebSocketServer, WebSocket } from 'ws';
 import path from 'path';
 
@@ -57,49 +53,3 @@ server.listen(process.env.PORT || 8080, () => {
 
     console.log("Example app listening at http://%s:%s", host, port)
 });
-
-//const express = require("express");
-//const http = require("http");
-//const { Server } = require("socket.io");
-
-
-//const app = express();
-//const server = http.createServer(app);
-//const io = new Server(server);
-
-//app.use(express.static("public")); // Serve static files (our frontend)
-
-//app.get('/', function (req, res) {
-//    res.sendFile(__dirname + '/client.html');
-//});
-
-//io.on("connection", (socket) => {
-//    console.log("A user connected:", socket.id);
-
-//    socket.on("offer", (offer) => {
-//        socket.broadcast.emit("offer", offer); // Send offer to other peers
-//    });
-
-//    socket.on("answer", (answer) => {
-//        socket.broadcast.emit("answer", answer); // Send answer to other peers
-//    });
-
-//    socket.on("candidate", (candidate) => {
-//        socket.broadcast.emit("candidate", candidate); // Send ICE candidate
-//    });
-
-//    socket.on("disconnect", () => {
-//        console.log("User disconnected:", socket.id);
-//    });
-//});
-
-////server.listen(3000, () => {
-////    console.log("Signaling server running on http://localhost:3000");
-////});
-
-//server.listen(process.env.PORT || 8081, function () {
-//    var host = server.address().address
-//    var port = server.address().port
-
-//    console.log("Example app listening at http://%s:%s", host, port)
-//});
